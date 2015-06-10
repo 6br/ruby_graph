7fe781b1b527f3477354d30061dec3c4f8d69d89
tate <tate@PCI-BIL.site>
2015-06-10 09:19:19 +0900

    RNG 1st Test Passed

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,5 +1,6 @@
 class RandomNodeGenerate
-
+  # @return [Object] NodeMapper of NodeMatrix
+ attr_reader :network
   # Description of method
   #
   # @param [Type] :nodes describe :nodes
@@ -7,21 +8,16 @@ class RandomNodeGenerate
   # @param [Type] :matrix or :list
   # @return [Object] Set of Nodes and Edges
   def initialize(nodes:,edges:,type:)
-    if :type == :matrix
-      @network = NodeMapper.new(:nodes)
-    elsif :type == :list
-	  @network = NodeMatrix.new(:nodes)
+    if type == :matrix
+      @network = NodeMapper.new(nodes)
+    elsif type == :list
+	  @network = NodeMatrix.new(nodes)
     end
 	edges.times do
-	  @network.add(random_generate(:edges))
+	  @network.add(random_generate(edges))
 	end
   end
 
-  def list
-    @network.list
-  end
-
-  private
     def random_generate(size)
       {first: rand(size),second: rand(size)}
     end
--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -1,12 +1,11 @@
 require "spec_helper"
 require_relative '../lib/graph'
 
-
 RSpec.describe RandomNodeGenerate do
   describe '#new' do
     it "yields 1000 size node" do
-      network = RandomNodeGenarate.new({nodes:1000,edges:1000,type:matrix})
-      expect(network.size).to eq 1000
+      network = RandomNodeGenerate.new(nodes: 1000,edges: 10000,type: :matrix)
+      expect(network.network.size).to eq 1000
     end
   end
 end
